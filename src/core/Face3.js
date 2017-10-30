9bf795862d505c255eb7ecd82658b0733cba4805
moraxy <moraxy@users.noreply.github.com>
2017-10-30 16:51:21 +0100

    Update Face3.js

--- src/core/Face3.js
+++ src/core/Face3.js
@@ -7,7 +7,7 @@ import { Vector3 } from '../math/Vector3.js';
  */
 
 function Face3( a, b, c, normal, color, materialIndex ) {
-console.log("face3foo 46");
+console.log("face3foo 47");
 	this.a = a;
 	this.b = b;
 	this.c = c;
